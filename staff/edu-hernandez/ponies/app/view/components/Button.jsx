da087ee3c80f9a13094988999b9e32aa6f54dcd2
trosdesuru <eduardhernandezventos@gmail.com>
2024-07-05 19:53:56 +0200

    add button component WIP; #70

---
+++ staff/edu-hernandez/ponies/app/view/components/Button.jsx
@@ -0,0 +1,23 @@
+const { Component } = React //const Component = React.Component
+
+class Button extends Component {
+    constructor() {
+        super()
+
+        this.state={ }
+    }
+
+    handleOnClick() {
+        this.props.onClicked();
+    }
+
+    render() {
+        return <button onClick={ this.handleOnClick.bind(this)}
+            type={ this.props.type }
+            className={ this.props.className }>
+            { this.props.text }
+        </button>
+    }
+}
+
+export default Button
