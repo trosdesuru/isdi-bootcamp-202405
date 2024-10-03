492c0e527bb327956d799a65336c6e25e0e09878
trosdesuru <eduardhernandezventos@gmail.com>
2024-10-03 15:11:31 +0200

    fixed error during function favEvents, #cities

--- staff/edu-hernandez/project/api/handlers/getAllFavEventsHandler.js
+++ staff/edu-hernandez/project/api/handlers/getAllFavEventsHandler.js
@@ -4,7 +4,7 @@ export default (req, res, next) => {
     const { userId } = req
 
     try {
-        logic.getAllMapEvents(userId)
+        logic.getAllFavEvents(userId)
             .then(events => res.json(events))
             .catch(error => next(error))
     } catch (error) {
